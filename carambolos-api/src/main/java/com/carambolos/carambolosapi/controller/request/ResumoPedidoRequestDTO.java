b3e7801aa86e78a7ab5e4faf5a36f1023ac7c6ee
Matheus Cantalejo <matheus.braga@sptech.school>
2025-05-28 20:19:25 -0300

    adicionando logica do valor para a resumo pedido

--- carambolos-api/src/main/java/com/carambolos/carambolosapi/controller/request/ResumoPedidoRequestDTO.java
+++ carambolos-api/src/main/java/com/carambolos/carambolosapi/controller/request/ResumoPedidoRequestDTO.java
@@ -7,10 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record ResumoPedidoRequestDTO (
-        @Schema(description = "Valor total do pedido", example = "120.50")
-        @NotNull
-        Double valor,
-
         @Schema(description = "Data prevista para entrega", example = "2024-12-25T15:00:00")
         LocalDateTime dataEntrega,
 
@@ -25,7 +21,6 @@ public record ResumoPedidoRequestDTO (
         }
 
         ResumoPedido resumoPedido = new ResumoPedido();
-        resumoPedido.setValor(request.valor);
         resumoPedido.setDataEntrega(request.dataEntrega);
         resumoPedido.setPedidoFornadaId(request.pedidoFornadaId);
         resumoPedido.setPedidoBoloId(request.pedidoBoloId);
