259fe58f3cca8bfe1f70c5574db7b069ea3f0d01
cantalejomatheus <matheus.braga@sptech.school>
2025-10-25 22:18:28 -0300

    feat: implement pagination for listarProdutoFornada in FornadaController and related classes

--- carambolos-api/src/main/java/com/carambolos/carambolosapi/infrastructure/persistence/jpa/ProdutoFornadaRepository.java
+++ carambolos-api/src/main/java/com/carambolos/carambolosapi/infrastructure/persistence/jpa/ProdutoFornadaRepository.java
@@ -1,6 +1,8 @@
 package com.carambolos.carambolosapi.infrastructure.persistence.jpa;
 
 import com.carambolos.carambolosapi.domain.entity.ProdutoFornada;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +15,9 @@ public interface ProdutoFornadaRepository extends JpaRepository<ProdutoFornada, 
     List<ProdutoFornada> findByCategoriaIn(List<String> categorias);
     boolean existsByProdutoAndIsAtivoTrue(String produto);
     boolean existsByProdutoAndIsAtivoTrueAndIdNot(String produto, Integer id);
+    Page<ProdutoFornada> findByCategoriaInAndIsAtivoTrue(Pageable page, List<String> categorias);
+    Page<ProdutoFornada> findByIsAtivoTrue(Pageable page);
+
 
     @Transactional
     @Modifying
