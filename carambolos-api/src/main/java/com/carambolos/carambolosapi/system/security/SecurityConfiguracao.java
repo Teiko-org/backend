9af2e6bfbb01d2a27b59397d9d2dfd7651bba05b
PajorVi <vinicius.marques@sptech.school>
2025-11-30 20:00:37 -0300

    finaliza refactor clean arch decoracao

--- carambolos-api/src/main/java/com/carambolos/carambolosapi/system/security/SecurityConfiguracao.java
+++ carambolos-api/src/main/java/com/carambolos/carambolosapi/system/security/SecurityConfiguracao.java
@@ -78,6 +78,7 @@ public class SecurityConfiguracao {
             new AntPathRequestMatcher("/adicionais", "GET"),
             // Permitir criação de decorações (imagens de referência) sem login
             new AntPathRequestMatcher("/decoracoes", "POST"),
+            new AntPathRequestMatcher("/decoracoes/**", "PUT"),
             // Permitir criação de resumo de pedido (WhatsApp) sem login
             new AntPathRequestMatcher("/resumo-pedido", "POST"),
 
